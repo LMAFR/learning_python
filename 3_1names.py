00fc83ebbc9738df367a567f76e9158249b7dacd
LMAFR <71094021+LMAFR@users.noreply.github.com>
2020-12-31 13:41:36 +0100

    Add files via upload

---
+++ 3_1names.py
@@ -0,0 +1,8 @@
+#In this script I will write a list with the name of some of my friends and later I will print each of their names one at a time.
+friends = ['Alberto','Irene','Gloria']
+#Alberto
+print(friends[0])
+#Gloria
+print(friends[-1])
+#Irene
+print(friends[1])
